8294709fa1bdc97af39f240a89bcdc2975eeea8d
Vojtěch Kudláček <vjtkudlacek@gmail.com>
2020-01-26 21:29:00 +0100

    Moved projects to storage

--- src/store/reducers.ts
+++ src/store/reducers.ts
@@ -1,8 +1,14 @@
 import * as types from './actionTypes';
+import { projects as projectList } from './defaultState';
 
 export const sceneChanging = (state: boolean = false, action: ReducerAction) => {
 	if (action.type === types.SET_SCREEN_CHANGING) {
 		return action.payload;
 	}
+	return state;
+};
+
+export const projects = (state: Array<Project> = projectList) => {
+	// Projects are read-only
 	return state;
 };
