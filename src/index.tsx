997009c81a1a038ae3cbad6ea5198132ffa068f6
Vojtěch Kudláček <vjtkudlacek@gmail.com>
2020-01-26 21:18:34 +0100

    Added google analytics

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,9 @@
 //? Core
 import React from 'react';
 import ReactDOM from 'react-dom';
+//? Services
 import { unregister } from 'services/serviceWorkers';
+import { startAnalyzing } from 'services/analytics';
 //? Components
 import App from 'App';
 
@@ -9,3 +11,5 @@ ReactDOM.render(<App />, document.getElementById('root'));
 
 // Unregister service workers - no chache for u
 unregister();
+// Some google analytics
+startAnalyzing();
